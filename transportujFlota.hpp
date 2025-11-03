e43e818e9b541d0a18e9d1304e00876a383e27f6
lziolek <lubomirziolek>
2025-11-03 11:18:14 +0100

    test

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,21 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia  {};
+    unsigned int przetransportowano = 0;
+    unsigned int zagle =0;
+    while (towar>przetransportowano){
+        Statek* s1 = stocznia();
+        przetransportowano +=s1->transportuj();
+
+        
+        
+        if(dynamic_cast <Zaglowiec*>(s1))
+        { zagle++; }
+        
+        delete s1;
+    }
+
+    return zagle;
+
 }
